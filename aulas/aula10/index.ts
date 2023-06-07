a7198db85603c9357ade398db37c43d40233698d
inkma <115982948+tomaslmz@users.noreply.github.com>
2023-06-07 17:08:02 -0300

    finish aula 10

---
+++ aulas/aula10/index.ts
@@ -0,0 +1,13 @@
+let age : number = 5;
+const firstName : string = 'felipe'
+const isValid : boolean = true;
+let idk : any = 5;
+
+idk = '12';
+idk = true;
+
+const ids : number[] = [1, 2, 3, 4, 5];
+const booleans : boolean[] = [true, false, true, false];
+const names : string[] = ['Felipe', 'Tomás', 'João', 'José'];
+
+console.log(age);
